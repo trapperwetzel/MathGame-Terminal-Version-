4cb1938c348ff96d9205446b415b908400d884ad
trapperwetzel <trapperwetzel@gmail.com>
2024-10-27 22:21:47 -0500

    - More refactoring

--- MenuMethods.cs
+++ MenuMethods.cs
@@ -18,176 +18,192 @@ namespace MathGame
 
             return name;
         }
-
         public static void AdditionGame(string message)
         {
-            // Generate random numbers for the problem given to the user.
+            Console.WriteLine($"{message}");
             Random rnd = new Random();
-            int number1 = rnd.Next(1, 9);
-            int number2 = rnd.Next(1, 9);
+            var score = 0;
+            int number1;
+            int number2;
 
-            // Takes user input, and outputs a question with the operator choosen.
-            int correctAnswer = 0;
-
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.WriteLine("                     Here is your question! ");
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.WriteLine($"                            {number1} + {number2}");
-            correctAnswer = Solve.Addition(number1, number2);
-
-            Console.WriteLine("Enter your answer below! ");
-            var userAnswerString = Console.ReadLine();
-            var userAnswer = int.Parse(userAnswerString);
-
-            if (userAnswer == correctAnswer)
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("------------------------");
-                Console.WriteLine("   Correct Answer! :-) ");
-                Console.WriteLine("------------------------");
-                Console.WriteLine("\n");
+                number1 = rnd.Next(1, 9);
+                number2 = rnd.Next(1, 9);
 
-            }
-            else if (userAnswer != correctAnswer)
-            {
-                Console.WriteLine("----------------------");
-                Console.WriteLine("   Wrong Answer! :-( ");
-                Console.WriteLine("----------------------");
-                Console.WriteLine("\n");
+
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("                     Here is your question! ");
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine($"                            {number1} + {number2}");
+                int correctAnswer = Solve.Addition(number1, number2);
+
+                Console.WriteLine("Enter your answer below! ");
+                var userAnswer = int.Parse(Console.ReadLine());
+
+
+                if (userAnswer == correctAnswer)
+                {
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine("   Correct Answer! :-) ");
+                    Console.WriteLine("------------------------");
+                    score++;
+                }
+                else if (userAnswer != correctAnswer)
+                {
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine("   Wrong Answer! :-( ");
+                    Console.WriteLine("----------------------");
+                }
+
+                if (i == 4)
+                {
+                    Console.WriteLine($"Game Over! Your score is {score}");
+                }
 
             }
 
         }
         public static void SubtractionGame(string message)
         {
-            // Generate random numbers for the problem given to the user.
+            Console.WriteLine($"{message}");
             Random rnd = new Random();
-            int number1 = rnd.Next(1, 9);
-            int number2 = rnd.Next(1, 9);
+            var score = 0;
+            int number1;
+            int number2;
 
-
-            // Takes user input, and outputs a question with the operator choosen.
-            int correctAnswer = 0;
-
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.WriteLine("                     Here is your question! ");
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.WriteLine($"                            {number1} - {number2}");
-            correctAnswer = Solve.Subtraction(number1, number2);
-
-            Console.WriteLine("Enter your answer below! ");
-            var userAnswerString = Console.ReadLine();
-            var userAnswer = int.Parse(userAnswerString);
-
-
-            if (userAnswer == correctAnswer)
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("------------------------");
-                Console.WriteLine("   Correct Answer! :-) ");
-                Console.WriteLine("------------------------");
-                Console.WriteLine("\n");
+                number1 = rnd.Next(1, 9);
+                number2 = rnd.Next(1, 9);
 
+
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("                     Here is your question! ");
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine($"                            {number1} - {number2}");
+                int correctAnswer = Solve.Subtraction(number1, number2);
+
+                Console.WriteLine("Enter your answer below! ");
+                var userAnswer = int.Parse(Console.ReadLine());
+
+
+                if (userAnswer == correctAnswer)
+                {
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine("   Correct Answer! :-) ");
+                    Console.WriteLine("------------------------");
+                    score++;
+                }
+                else if (userAnswer != correctAnswer)
+                {
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine("   Wrong Answer! :-( ");
+                    Console.WriteLine("----------------------");
+                }
+
+                if (i == 4)
+                {
+                    Console.WriteLine($"Game Over! Your score is {score}");
+                }
             }
-
-            else if (userAnswer != correctAnswer)
-            {
-                Console.WriteLine("----------------------");
-                Console.WriteLine("   Wrong Answer! :-( ");
-                Console.WriteLine("----------------------");
-                Console.WriteLine("\n");
-            }
-
-
         }
         public static void MultiplicationGame(string message)
         {
-            // Generate random numbers for the problem given to the user.
+            Console.WriteLine($"{message}");
             Random rnd = new Random();
-            int number1 = rnd.Next(1, 9);
-            int number2 = rnd.Next(1, 9);
+            var score = 0;
+            int number1;
+            int number2;
 
-            // Takes user input, and outputs a question with the operator choosen.
-
-            int correctAnswer = 0;
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.WriteLine("                     Here is your question! ");
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.WriteLine($"                            {number1} * {number2}");
-            correctAnswer = Solve.Multiplication(number1, number2);
-
-            Console.WriteLine("Enter your answer below! ");
-            var userAnswerString = Console.ReadLine();
-            var userAnswer = int.Parse(userAnswerString);
-
-
-            if (userAnswer == correctAnswer)
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("------------------------");
-                Console.WriteLine("   Correct Answer! :-) ");
-                Console.WriteLine("------------------------");
-                Console.WriteLine("\n");
+                number1 = rnd.Next(1, 9);
+                number2 = rnd.Next(1, 9);
+
+
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("                     Here is your question! ");
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine($"                            {number1} * {number2}");
+                int correctAnswer = Solve.Multiplication(number1, number2);
+
+                Console.WriteLine("Enter your answer below! ");
+                var userAnswer = int.Parse(Console.ReadLine());
+
+
+                if (userAnswer == correctAnswer)
+                {
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine("   Correct Answer! :-) ");
+                    Console.WriteLine("------------------------");
+                    score++;
+                }
+                else if (userAnswer != correctAnswer)
+                {
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine("   Wrong Answer! :-( ");
+                    Console.WriteLine("----------------------");
+                }
+
+                if (i == 4)
+                {
+                    Console.WriteLine($"Game Over! Your score is {score}");
+                }
+
 
             }
 
-            else if (userAnswer != correctAnswer)
-            {
-                Console.WriteLine("----------------------");
-                Console.WriteLine("   Wrong Answer! :-( ");
-                Console.WriteLine("----------------------");
-                Console.WriteLine("\n");
-
-            }
 
         }
         public static void DivisionGame(string message)
         {
-            // Generate random numbers for the problem given to the user.
+            Console.WriteLine($"{message}");
             Random rnd = new Random();
-            int number1 = rnd.Next(1, 9);
-            int number2 = rnd.Next(1, 9);
+            var score = 0;
+            int number1;
+            int number2;
 
-
-            // Takes user input, and outputs a question with the operator choosen.
-            int correctAnswer = 0;
-
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.WriteLine("                     Here is your question! ");
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.WriteLine($"                            {number1} / {number2}");
-            correctAnswer = Solve.Division(number1, number2);
-
-            Console.WriteLine("Enter your answer below! ");
-            var userAnswerString = Console.ReadLine();
-            var userAnswer = int.Parse(userAnswerString);
-
-
-            if (userAnswer == correctAnswer)
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("------------------------");
-                Console.WriteLine("   Correct Answer! :-) ");
-                Console.WriteLine("------------------------");
-                Console.WriteLine("\n");
+                number1 = rnd.Next(1, 9);
+                number2 = rnd.Next(1, 9);
+
+
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("                     Here is your question! ");
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine($"                            {number1} / {number2}");
+                int correctAnswer = Solve.Division(number1, number2);
+
+                Console.WriteLine("Enter your answer below! ");
+                var userAnswer = int.Parse(Console.ReadLine());
+
+
+                if (userAnswer == correctAnswer)
+                {
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine("   Correct Answer! :-) ");
+                    Console.WriteLine("------------------------");
+                    score++;
+                }
+                else if (userAnswer != correctAnswer)
+                {
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine("   Wrong Answer! :-( ");
+                    Console.WriteLine("----------------------");
+                }
+
+                if (i == 4)
+                {
+                    Console.WriteLine($"Game Over! Your score is {score}");
+                }
 
 
             }
 
-            else if (userAnswer != correctAnswer)
-            {
-                Console.WriteLine("----------------------");
-                Console.WriteLine("   Wrong Answer! :-( ");
-                Console.WriteLine("----------------------");
-                Console.WriteLine("\n");
-
-
-            }
 
         }
-
-
-
-
-
-
-
-
     }
 }
+
